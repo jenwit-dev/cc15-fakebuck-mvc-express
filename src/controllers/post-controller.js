99e448aa795c6ead763b56765501ba0be6587073
jenwit-dev <johnny.js19@gmail.com>
2026-01-29 21:19:23 +0700

    update feature createPost

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -41,9 +41,26 @@ exports.createPost = async (req, res, next) => {
       data.message = message;
     }
 
-    await prisma.post.create({ data });
+    const post = await prisma.post.create({
+      data,
+      include: {
+        user: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            profileImage: true,
+          },
+        },
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+      },
+    });
 
-    res.status(201).json({ message: "post created" });
+    res.status(201).json({ message: "post created", post });
   } catch (err) {
     next(err);
   } finally {
